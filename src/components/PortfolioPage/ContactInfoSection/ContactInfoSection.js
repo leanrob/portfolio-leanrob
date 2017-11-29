399de41b910d68ad23105ba6d7dbfd583b3a06e2
Rob Byrne <rob@leanrob.com>
2017-11-28 22:17:34 -0800

    Styling edits to skills text
    Styling changes to the skills text
    Styling skills list to be awesome
    Adding an image (to be imput with props next) for eaxch skill
    Looking and running good

--- src/components/PortfolioPage/ContactInfoSection/ContactInfoSection.js
+++ src/components/PortfolioPage/ContactInfoSection/ContactInfoSection.js
@@ -5,21 +5,7 @@ import './contactInfoSection.css'
 const ContactInfoSection = () => {
 	return (
 		<div className="main-content" id="contactInfo">
-			<h1>Contact Infoe</h1>
-			<h2>Software Developer. I get shit done</h2>
-			<h1>Rob Byrne</h1>
-			<h2>Software Developer. I get shit done</h2>
-			<h1>Rob Byrne</h1>
-			<h2>Software Developer. I get shit done</h2>
-			<h1>Rob Byrne</h1>
-			<h2>Software Developer. I get shit done</h2>
-			<h1>Rob Byrne</h1>
-			<h2>Software Developer. I get shit done</h2>
-			<h1>Rob Byrne</h1>
-			<h2>Software Developer. I get shit done</h2>
-			<h1>Rob Byrne</h1>
-			<h2>Software Developer. I get shit done</h2>
-			<h1>Section End</h1>
+			<h1 className="contact-heading">Contact Info</h1>
 		</div>
 	)
 };
--- src/components/PortfolioPage/SkillsSection/SkillDescription.js
+++ src/components/PortfolioPage/SkillsSection/SkillDescription.js
@@ -1,22 +1,25 @@
 import React from "react"
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+
+import reactlogo from "../../../assets/images/react-redux.jpeg"
 
 const MainPoints = ({mainPoints}) => {
 	let points = [];
-	mainPoints.forEach(function(value) {
-		points.push(<li>{value}</li>)
-	});
+	for (let index = 0; index < mainPoints.length; index++) {
+		points.push(<li><span>{index}.</span><p>{mainPoints[index]}</p></li>)
+	}
 	return points;
 };
 
 const SkillDescription = ({title, description, mainPoints, usedIn}) => {
 	return (
-		<div>
-			<h2 className="skill-heading">{title}</h2>
+		<div className="skill">
+			<img className="skill-image" src={reactlogo} alt="React" />
+			<h3 className="skill-heading">{title}</h3>
 			<p className="skill-description">{description}</p>
-			<ul>
+			<ol>
 				<MainPoints mainPoints={mainPoints}/>
-			</ul>
+			</ol>
 			<p className="skill-usedIn">{usedIn}</p>
 		</div>
 	)
